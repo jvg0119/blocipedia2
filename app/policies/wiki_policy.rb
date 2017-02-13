1541579bb419568b591ad007cddd8e0d00653384
Joseph <jvg_0119@yahoo.com>
2017-02-13 10:03:25 -0800

    private/public view adjustments

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -15,7 +15,6 @@ class WikiPolicy < ApplicationPolicy
     user.present? && (record.user == user || user.admin?)
   end
 
-
   class Scope
     attr_reader :user, :scope
 
@@ -25,21 +24,27 @@ class WikiPolicy < ApplicationPolicy
     end
 
     def resolve
-
-     if user.try(:admin?)
+      wikis = []
+      all_wikis = scope.all
+      if user.try(:admin?)
+        # all_wikis.each do |wiki| 
+        #   wikis << wiki
         wikis = scope.all
       elsif user.try(:premium?) 
-         scope.public_wikis + scope.private_wikis.where(user: user) + user.wiki_collaborations
-         # public wikis + private wikis owned by current_user + wikis the user is collaborating with
-
-      elsif user.try(:standard?)
-        scope.public_wikis + user.wiki_collaborations
-
+        all_wikis.each do |wiki|
+          if !wiki.private? || wiki.user == user || wiki.wiki_collaborators.include?(user)
+            wikis << wiki
+          end
+        end
       else
-        scope.public_wikis # guest
-      end  
-    end   # resolve
-  end   # scope
+        all_wikis.each do |wiki| # standard & guests
+          if !wiki.private? || wiki.wiki_collaborators.include?(user)
+            wikis << wiki
+          end
+        end
+      end # if user.try(:admin?)
+      wikis
+    end  # resolve
+  end # scope
 end
-
 
